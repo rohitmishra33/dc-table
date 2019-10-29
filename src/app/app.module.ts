05fb80e2d54ff47b32fd25793253042e28ae6ba6
Rohit Mishra <callrohit33@gmail.com>
2019-10-29 21:29:24 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EpisodeTableComponent } from './episode-table/episode-table.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EpisodeTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/episode-table/episode-table.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+    selector: 'app-episode-table',
+    templateUrl: './episode-table.component.html',
+    styleUrls: ['./episode-table.component.css']
+})
+export class EpisodeTableComponent implements OnInit {
+
+    private REST_URL = "http://34.69.234.16:3000/";
+
+    episodes = [];
+
+    constructor(private httpClient: HttpClient) { }
+
+    public getEpisodeList() {
+        return this.httpClient.get(this.REST_URL);
+    }
+
+    ngOnInit() {
+        this.getEpisodeList().subscribe((data: any[]) => {
+            this.episodes = data;
+            console.log(data);
+        });
+    }
+
+}
