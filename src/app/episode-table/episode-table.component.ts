aa585a284c2c2ed540205b598414b0cfbb2c0694
Rohit Mishra <callrohit33@gmail.com>
2019-11-02 21:12:35 +0530

    Changing the date format

--- src/app/episode-table/episode-table.component.ts
+++ src/app/episode-table/episode-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import * as moment from 'moment';
 
 @Component({
     selector: 'app-episode-table',
@@ -11,7 +12,7 @@ export class EpisodeTableComponent implements OnInit {
     private REST_URL = "http://34.69.234.16:3000/";
 
     episodes = [];
-    currentDate = (new Date()).toISOString().split('T')[0];
+    currentDate = moment().format('YYYY-MM-DD');
 
     constructor(private httpClient: HttpClient) { }
 
@@ -21,8 +22,17 @@ export class EpisodeTableComponent implements OnInit {
 
     ngOnInit() {
         this.getEpisodeList().subscribe((data: any[]) => {
+            data.forEach((e) => {
+                if (e['Original Air Date']) {
+                    if (e['Original Air Date'].length > 7) {
+                        e['Formatted Date'] = moment(e['Original Air Date']).format('MMMM DD, YYYY');
+                    } else {
+                        e['Formatted Date'] = moment(e['Original Air Date']).format('MMMM, YYYY');
+                    }
+                }
+            });
             this.episodes = data;
-            // console.log(data);
+            // console.log(this.episodes);
         });
     }
 
